4903783f20d20521a70c7a40fae60480896863f3
ANNA MARONE <annamarone72@gmail.com>
2024-03-04 16:55:41 +0000

    fonctionnalité affichage

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php 
-$host ="localhost";
-$usernameer = "root";
-$dbname = "gestion_des_etudiants";
+$host ="localhost:3307";
+$username = "root";
+$dbname = "gestion_des_Etudiant";
 $password = "";
 
 try{
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username,$password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo'tout est bon';
     }
